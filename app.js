12597b8c91c70de15ccca2642d43c353395c739b
nicholasjativa <nicholasjativa@outlook.com>
2017-04-28 03:21:37 -0400

    Must fix JSON parsing of body

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ var express = require('express');
 var http = require('http');
 var twilioClient = require('./twilioClient');
 var app = express();
+var router = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+twilioClient.testToAndroid();
 
+router.post('/twilio', function(req, res) {
+    var jsonSmsInfo = req.body;
+    var jsonSmsBody = JSON.parse('"' + jsonSmsInfo.Body + '"');
+    transformMessageFromAndroid(jsonSmsBody);
+    res.send(400);
+});
 
-twilioClient.sendSms();
+function transformMessageFromAndroid(bareSms) {
+    var messageSender = bareSms.phoneNumber;
+    var messageBody = bareSms.body;
+    var messageForSlack = "Received message from: " + messageSender + "\nBody: " + messageBody;
+    console.log(messageForSlack);
+}
 
-app.listen(8000, function() {
+app.use(router);
+//twilioClient.create();
+
+app.listen(5000, function() {
     console.log('Amadeus server listening on :8000');
 });
--- twilioClient.js
+++ twilioClient.js
@@ -1,14 +1,22 @@
-var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+var twilio = require('twilio');
+var client = new twilio.RestClient();
 
-module.exports.sendSms = function(to, message) {
+/*module.exports.sendToAndroid = function (smsSender, smsBody) {
     client.messages.create({
-        body: "Hey",
-        to: "+16313360360",
-        from: '+15165950512' 
-    }, function( err, data) {
-        if (err) console.error(err);
-        else {
-            console.log('Message sent!');
-        }
-    });
-};
+        body: 'Received message from: ' + smsSender + '\nBody: ' + smsBody,
+        to: '+16313360360',
+        from: '+15165950512'
+    }, function (error, message) {
+        console.log(message.sid);
+    }); 
+}*/
+
+module.exports.testToAndroid = function () {
+    client.messages.create({
+        body: 'Hello, World!',
+        to: '+16313360360',
+        from: '+15165950512'
+    }, function (error, message) {
+        ///console.log(message.sid);
+    }); 
+}
